9afaae576ee5ae1eea699d5da88e2b9b2e198dcb
Renisson Silva <renissonx@gmail.com>
2020-05-22 16:03:11 -0300

    Exemplo de estado - useState

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [counter,setCounter] = useState(0);
+
+  function incrementCounter(){
+    setCounter(counter+1);
+  }
+
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
