7131b3496ceebc05a61f19069686f936eca95477
Renisson Silva <renissonx@gmail.com>
2020-05-23 17:03:15 -0300

    Adicionando marcador na posição do usuário

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main(){
@@ -32,12 +32,25 @@ function Main(){
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map}/>
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -7.9067864, longitude: -34.826833}}>
+      <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/40182898?s=460&u=c6da2d327059b417b818e0b123764b0165cca24e&v=4'}}/>
+      </Marker>
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
   map:{
-    flex: 1
+    flex: 1,
+  },
+  avatar: {
+    width:54,
+    height:54,
+    borderRadius:4,
+    borderWidth:4,
+    borderColor:'#FFF',
   },
 })
 
